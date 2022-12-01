b58f9a1487a4a3375e70af75296dcc57d750e619
chaeminkim <30472347+chaeminkimKWU@users.noreply.github.com>
2022-12-01 22:58:19 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,102 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 14:05:06 2022
+
+@author: coals
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
 import numpy as np
+from entropy.MDE.MDE import MDE
+from entropy.MCRDE.MCRDE import MCRDE
+from entropy.MFDE.MFDE import MFDE
 from scipy import io
 from scipy.stats import ranksums
-
-from entropy.MCRDE.MCRDE import MCRDE
-
-from utils.plot_Entropy import plot_Entropy
 from utils.write_txt import write_txt
+from utils.plot_Entropy import plot_Entropy
+
+### Extract Features using entropy methods
+
+### input parameters
+
+# RRIs_data path
+RRIs_CHF_path = './sample_data/RRIs_CHF_1000'     # CHF(Congestive heart failure) -> 울혈성 심부전 피험자 14명의 RRIs data
+RRIs_HEALTHY_path = './sample_data/RRIs_HEALTHY_1000'     # HEALTHY                         -> 건강한 피험자 16명의 RRIs data
+
+# Multiscale Entropy method parameter
+Entropy_method = 'MCRDE'
+Entropy_method_dict = { 'MDE':MDE, 'MCRDE':MCRDE, 'MFDE':MFDE }
+
+# Multiscale Entropy average, standard deviation data path
+avg_Entropy_chf_path = './results/avg_' + Entropy_method + '_chf.txt'            # path of CHF subjects Multiscale Entropy avg data 
+avg_Entropy_healthy_path = './results/avg_' + Entropy_method + '_healthy.txt'    # path of HEALTHY subjects Multiscale Entropy avg data
+std_Entropy_chf_path = './results/std_' + Entropy_method + '_chf.txt'            # path of CHF subjects Multiscale Entropy std data 
+std_Entropy_healthy_path = './results/std_' + Entropy_method + '_healthy.txt'    # path of HEALTHY subjects Multiscale Entropy std data
 
 
-RRIs_CHF_path = './sample_data/RRIs_CHF_1000'                        # CHF(= Congestive Heart Failure) -> 울혈성 심부전 피험자 14명의 RRIs data
-RRIs_HEALTHY_path = './sample_data/RRIs_HEALTHY_1000'                # HEALTHY                         -> 건강한 피험자 14명 RRIs data
+if Entropy_method == 'MDE':
+    # MDE parameters
+    N = 1000    # RRIs data length (default:1000) 
+    m = 3       # embeding dimension (default:3)
+    c = 6       # number of class (default:6)
+    tau = 1     # delay factor (default:1)
+    scale = 25  # scale factor (default:25)
+    params_dict = { 'N':N,'m':m,'c':c,'tau':tau,'scale':scale }     # MDE parameter (type:dict)
 
-avg_mcrde_chf_path = './results/avg_mcrde_chf.txt'      # path of CHF subjects MCRDE avg data
-avg_mcrde_healthy_path = './results/avg_mcrde_healthy.txt'  # path of HEALTHY subjects MCRDE avg data
-std_mcrde_chf_path = './results/std_mcrde_chf.txt'      # path of CHF subjects MCRDE std data
-std_mcrde_healthy_path = './results/std_mcrde_healthy.txt'  # path of HEALTHY subjects MCRDE std data
+elif Entropy_method == 'MCRDE':
+    # MCRDE parameters
+    N = 1000    # RRIs data length (default:1000)
+    m = 3       # embeding dimension (default:3)
+    c = 6       # number of class (default:6)
+    tau = 1     # delay factor (default:1)
+    scale = 25  # scale factor (default:25)
+    params_dict = { 'N':N,'m':m,'c':c,'tau':tau,'scale':scale }     # MCRDE parameter (type:dict)
 
-# MCRDE parameters
-N = 1000   # RRIs data length
-m = 3   # embeding dimension
-c = 6   # number of class
-tau = 1    # delay factor
-scale = 25    # scale factor
+elif Entropy_method == 'MFDE':
+     # MFDE parameters
+     N = 1000    # RRIs data length (default:1000)
+     m = 4       # embeding dimension (default:4)
+     c = 4       # number of class (default:4)
+     tau = 1     # delay factor (default:1)
+     scale = 25  # scale factor (default:25)
+     params_dict = { 'N':N,'m':m,'c':c,'tau':tau,'scale':scale }     # MFDE parameter (type:dict)
+     
+
 
 # plot parameters
-show_fig = True                                            # plot draw flag
-subject = np.array(['CHF', 'HEALTHY'])                     # 비교군: CHF, Healthy subject
-plt_color = np.array(['red','blue'])                          # CHF MCRDE plot: 빨간색, Healthy MCRDE plot: 파란색
+show_fig = True     # plot draw flag
+subject = np.array(['Congestive heart failure (CHF)', 'HEALTHY'])  # 비교군: CHF, Healthy subjects
+plt_color = np.array(['red','blue'])    # CHF plot color , Healthy plot color 
+plt_marker = np.array(['o','^'])        # CHF plot marker, Healthy plot marker
+plt_linestyle = np.array(['-',':'])     # CHF plot linestyle, Healthy plot linestyle
+
 
 ### Sample data
 
 # Load RRIs data(type:numpy array, row:subjects, col:RRIs data of subjects)
 # CHF data
 RRIs_CHF_data = io.loadmat(RRIs_CHF_path)
-RRIs_CHF_1000 = RRIs_CHF_data['RRIs_CHF_1000']              # Load RRIs data of CHF(length=1000)
+RRIs_CHF_1000 = RRIs_CHF_data['RRIs_CHF_1000']      # Load RRIs data of CHF(length=1000) 
 
 # Healthy data
 RRIs_HEALTHY_data = io.loadmat(RRIs_HEALTHY_path)
 RRIs_HEALTHY_1000 = RRIs_HEALTHY_data['RRIs_HEALTHY_1000']  # Load RRIs data of HEALTHY(length=1000)
 
-n_s = len(RRIs_CHF_1000[:,0])                             # number of subject(CHF,HEALTHY)
+n_s   = len(RRIs_CHF_1000[:,0])          # number of CHF,Healthy subjects (14) 
+ 
 
-### output data
+### output data 
 
-# MCRDE about RRIs datas of CHF subjects
-mcrde_chf = np.zeros((n_s, scale))                       # row: subject, col: scale
-# MCRDE about RRIs datas of Healthy subjects
-mcrde_healthy = np.zeros((n_s, scale))                       # row: subject, col: scale
+# Multiscale Entropy Value about RRIs datas of CHF subjects
+Entropy_chf     = np.zeros((n_s,scale))   # row: subject, col: scale factor
+# Multiscale Entropy Value about RRIs datas of Healthy subjects
+Entropy_healthy = np.zeros((n_s,scale))   # row: subject, col: scale factor
          
 
-### Calculate MultiScale Entropy, p-value(Wilcoxon rank sum test)
+### Calculate MultiScale Entropy, p-value (Wilcoxon rank sum test)
+                      
 for i in range(n_s):
-    
-    # Calculate MCRDE about RRIs data of CHF subjects
-    mcrde_chf[i] = MCRDE(RRIs_CHF_1000[i, :N], m, c, tau, scale)
-    # Calculate MCRDE about RRIs data of Healthy subjects
-    mcrde_healthy[i] = MCRDE(RRIs_HEALTHY_1000[i, :N], m, c, tau, scale)
+    # Calculate Multiscale Entropy value of RRIs data of CHF, Healthy subjects
+        Entropy_chf[i] = Entropy_method_dict[Entropy_method](RRIs_CHF_1000[i,:N], params_dict)
+        Entropy_healthy[i] = Entropy_method_dict[Entropy_method](RRIs_HEALTHY_1000[i,:N], params_dict)
 
+# Calculate Multiscale Entropy average value
+avg_Entropy_chf     = np.mean(Entropy_chf,axis=0)       
+avg_Entropy_healthy = np.mean(Entropy_healthy,axis=0)
 
-# Calculate MCRDE average value
-avg_mcrde_chf = np.mean(mcrde_chf, axis=0)
-avg_mcrde_healthy = np.mean(mcrde_healthy, axis=0)
+# Calculate Multiscale Entropy std value
+std_Entropy_chf     = np.std(Entropy_chf,axis=0)
+std_Entropy_healthy = np.std(Entropy_healthy,axis=0)
 
-
-# Calculate MCRDE std value
-std_mcrde_chf = np.std(mcrde_chf, axis=0)
-std_mcrde_healthy = np.std(mcrde_healthy, axis=0)
-
-
-# Calculate p-value(between CHF and Healthy subjects)
-# p-value<0.05은 두 데이터(CHF, Healthy)가 서로 독립적인(=유의미한) 데이터라는 것을 보여준다. 
-pCHF_HT = np.zeros(scale)              # p-value between CHF and Healthy subjects(scale 1~25)
+# Calculate p-value (between CHF and Healthy subjects)
+# p-value<0.05일 때, 유의 수준 (significant) 에서 두 데이터(CHF, Healthy)가 서로 독립적 
+pCHF_HT = np.zeros(scale)             # p-value between CHF and Healthy subjects(scale 1~25)
 for i in range(scale):
-    s, pCHF_HT[i] = ranksums(mcrde_chf[:,i],mcrde_healthy[:,i])
+    s, pCHF_HT[i] = ranksums(Entropy_chf[:,i],Entropy_healthy[:,i]) 
 
 
 
-### MCRDE 평균, 표준편차 출력 파일을 text 파일에 저장한다.
-# CHF, Healthy subjects는 각각 모두 14명이다.
-# MultiScale Entropy method를 통해 scale(1~25)에서의 14명의 Entropy 평균, 표준 편차를 구한다. 
-# 사용한 Entropy 기법은 MultiScale Cumulative Residual Dispersion Entropy이다.
+### Multiscale Entropy 평균, 표준편차 값을 text 파일에 저장 
 # index: scale, data: avg, std
 
-write_txt(avg_mcrde_chf_path, avg_mcrde_healthy_path,
-          std_mcrde_chf_path, std_mcrde_healthy_path,
-          avg_mcrde_chf, avg_mcrde_healthy,
-          std_mcrde_chf, std_mcrde_healthy )
+write_txt(Entropy_method, scale,
+          avg_Entropy_chf_path, avg_Entropy_healthy_path,
+          std_Entropy_chf_path, std_Entropy_healthy_path,
+          avg_Entropy_chf, avg_Entropy_healthy,
+          std_Entropy_chf, std_Entropy_healthy )
 
 # show the result figure
 if show_fig == True:
-    Entropy = plot_Entropy(subject,plt_color,
-                            avg_mcrde_chf,
-                            avg_mcrde_healthy,
-                            std_mcrde_chf,
-                            std_mcrde_healthy,
-                            scale,
-                            pCHF_HT)
-    
+    Entropy = plot_Entropy(Entropy_method,
+                            subject,
+                            plt_color,
+                            plt_marker,
+                            plt_linestyle,
+                            avg_Entropy_chf,
+                            avg_Entropy_healthy,
+                            std_Entropy_chf,
+                            std_Entropy_healthy,
+                            pCHF_HT,
+                            params_dict)
+
 
 
 
